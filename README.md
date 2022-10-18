c0e9115c87cf30179da6f30fc317aa7a7580ca14
rardx <rardx@cyberard.com>
2022-10-18 15:50:47 +0800

    update with latest matches

--- README.md
+++ README.md
@@ -6,26 +6,47 @@ Currently in progress.
 2. Run the ti11predictions.ipynb file.
 3. Results will then be shown.
 
-*as of 2022-10-17 23:35:58 PHT*
+*as of 2022-10-18 15:49:00 PHT*
 ## Heroes
 ### Most Picked
- - Shadow Fiend : 57.0
- - Marci : 57.0
+ - Marci : 69.0
 ### Most banned
- - Enigma : 97.0
+ - Enigma : 108.0
 ### Highest Winrate
- - Crystal Maiden : 77.42
+ - Storm Spirit : 80.0
 ### Highest Average Kills
- - Bloodseeker : 13.0
+ - Bloodseeker : 11.71
 ### Highest Average Assists
- - Earth Spirit : 19.2
+ - Earth Spirit : 20.17
 ### Lowest Average deaths
  - Lifestealer : 2.0
 ### Highest Average Last hits
- - Naga Siren : 518.38
+ - Naga Siren : 542.48
 ### Highest Average XPM
  - Arc Warden : 783.0
 ### Most Kills in a game
  - Bloodseeker : 25.0
 ### Most Last Hits in a game
  - Naga Siren : 1196.0
+## Players
+### Highest Average Kills
+ - Pakazs : 9.71
+### Highest Average Assists
+ - Saksa : 17.18
+### Lowest Average Deaths
+ - Nightfall 凛 : 1.83
+### Highest Average Last Hits
+ - JACKBOYS : 444.11
+### Highest Average XPM
+ - Pakazs : 796.29
+### Most Kills in a game
+ - Crystallis : 25.0
+### Most Assists in a game
+ - TIMS : 39.0
+ - RodjER : 39.0
+### Most Last Hits in a game
+ - Monet : 1196.0
+### Most GPM in a game
+ - Yuragi : 1034.0
+### Most Different Heroes
+ - Yuragi : 16.0
--- convert.py
+++ convert.py
@@ -1,17 +1,24 @@
 import pandas as pd, datetime
 
 
-
-hero_list = pd.read_json('hero_list.json')
-hero_list = hero_list.to_dict('records')
-timenow = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-readme_file = open("README.md","w")
-readme = '# DOTA2 TI 2022 BATTLEPASS PREDICTIONS TRACKER\n I made this repo to track progress on the battlepass prediction.\nCurrently in progress.\n### To use:\n1. Run the getmatches.ipynb file.\n2. Run the ti11predictions.ipynb file.\n3. Results will then be shown.'
-readme += f'\n\n*as of {timenow} PHT*'
-readme += f'\n## Heroes'
-for i in hero_list:
-    readme += f'\n### {i["title"]}'
-    for j in i['result']:
-        readme += f"\n - {j['name']} : {round(j['value'],2)}"
-readme_file.write(readme)
-readme_file.close()
+def create_readme():
+    hero_list = pd.read_json('hero_list.json')
+    hero_list = hero_list.to_dict('records')
+    player_list = pd.read_json('player_list.json')
+    player_list = player_list.to_dict('records')
+    timenow = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    readme_file = open("README.md","w",encoding="utf-8")
+    readme = '# DOTA2 TI 2022 BATTLEPASS PREDICTIONS TRACKER\n I made this repo to track progress on the battlepass prediction.\nCurrently in progress.\n### To use:\n1. Run the getmatches.ipynb file.\n2. Run the ti11predictions.ipynb file.\n3. Results will then be shown.'
+    readme += f'\n\n*as of {timenow} PHT*'
+    readme += f'\n## Heroes'
+    for i in hero_list:
+        readme += f'\n### {i["title"]}'
+        for j in i['result']:
+            readme += f"\n - {j['name']} : {round(j['value'],2)}"
+    readme += f'\n## Players'
+    for i in player_list:
+        readme += f'\n### {i["title"]}'
+        for j in i['result']:
+            readme += f"\n - {j['name']} : {round(j['value'],2)}"
+    readme_file.write(readme)
+    readme_file.close()
